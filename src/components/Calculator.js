efe19cc8ea64d5ed645aa8e9cb05b93bfbbce964
Omar Farouq <ofarouq310@gmail.com>
2022-12-16 15:41:22 +0200

    Update script files for new sections

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -20,31 +20,36 @@ class Calculator extends React.Component {
   render() {
     const { total, next, operation } = this.state;
     return (
-      <div className="calculator">
-        <div id="calculator-display">
-          {total}
-          {operation}
-          {next}
+      <div className="main-container">
+        <div id="header">
+          <h1>Let&apos;s do some math!</h1>
         </div>
-        <button type="button" onClick={this.handleClick} className="calculator-key" id="key-clear">AC</button>
-        <button type="button" onClick={this.handleClick} className="calculator-key" id="key-sign">+/-</button>
-        <button type="button" onClick={this.handleClick} className="calculator-key" id="key-percent">%</button>
-        <button type="button" onClick={this.handleClick} className="calculator-key orange-key" id="key-division">÷</button>
-        <button type="button" onClick={this.handleClick} className="calculator-key" id="key-7">7</button>
-        <button type="button" onClick={this.handleClick} className="calculator-key" id="key-8">8</button>
-        <button type="button" onClick={this.handleClick} className="calculator-key" id="key-9">9</button>
-        <button type="button" onClick={this.handleClick} className="calculator-key orange-key" id="key-multiply">x</button>
-        <button type="button" onClick={this.handleClick} className="calculator-key" id="key-4">4</button>
-        <button type="button" onClick={this.handleClick} className="calculator-key" id="key-5">5</button>
-        <button type="button" onClick={this.handleClick} className="calculator-key" id="key-6">6</button>
-        <button type="button" onClick={this.handleClick} className="calculator-key orange-key" id="key-minus">-</button>
-        <button type="button" onClick={this.handleClick} className="calculator-key" id="key-1">1</button>
-        <button type="button" onClick={this.handleClick} className="calculator-key" id="key-2">2</button>
-        <button type="button" onClick={this.handleClick} className="calculator-key" id="key-3">3</button>
-        <button type="button" onClick={this.handleClick} className="calculator-key orange-key" id="key-plus">+</button>
-        <button type="button" onClick={this.handleClick} className="calculator-key" id="key-0">0</button>
-        <button type="button" onClick={this.handleClick} className="calculator-key" id="key-dot">.</button>
-        <button type="button" onClick={this.handleClick} className="calculator-key orange-key" id="key-equals">=</button>
+        <div className="calculator">
+          <div id="calculator-display">
+            {total}
+            {operation}
+            {next}
+          </div>
+          <button type="button" onClick={this.handleClick} className="calculator-key" id="key-clear">AC</button>
+          <button type="button" onClick={this.handleClick} className="calculator-key" id="key-sign">+/-</button>
+          <button type="button" onClick={this.handleClick} className="calculator-key" id="key-percent">%</button>
+          <button type="button" onClick={this.handleClick} className="calculator-key orange-key" id="key-division">÷</button>
+          <button type="button" onClick={this.handleClick} className="calculator-key" id="key-7">7</button>
+          <button type="button" onClick={this.handleClick} className="calculator-key" id="key-8">8</button>
+          <button type="button" onClick={this.handleClick} className="calculator-key" id="key-9">9</button>
+          <button type="button" onClick={this.handleClick} className="calculator-key orange-key" id="key-multiply">x</button>
+          <button type="button" onClick={this.handleClick} className="calculator-key" id="key-4">4</button>
+          <button type="button" onClick={this.handleClick} className="calculator-key" id="key-5">5</button>
+          <button type="button" onClick={this.handleClick} className="calculator-key" id="key-6">6</button>
+          <button type="button" onClick={this.handleClick} className="calculator-key orange-key" id="key-minus">-</button>
+          <button type="button" onClick={this.handleClick} className="calculator-key" id="key-1">1</button>
+          <button type="button" onClick={this.handleClick} className="calculator-key" id="key-2">2</button>
+          <button type="button" onClick={this.handleClick} className="calculator-key" id="key-3">3</button>
+          <button type="button" onClick={this.handleClick} className="calculator-key orange-key" id="key-plus">+</button>
+          <button type="button" onClick={this.handleClick} className="calculator-key" id="key-0">0</button>
+          <button type="button" onClick={this.handleClick} className="calculator-key" id="key-dot">.</button>
+          <button type="button" onClick={this.handleClick} className="calculator-key orange-key" id="key-equals">=</button>
+        </div>
       </div>
     );
   }
