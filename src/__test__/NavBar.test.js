1a15e77450296990c40c645565fa361974f33170
Omar Farouq <ofarouq310@gmail.com>
2023-01-19 11:42:43 +0200

    Update NavBar.test.js

--- src/__test__/NavBar.test.js
+++ src/__test__/NavBar.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/Navbar.js';
 
 describe('check Navbar', () => {
   const tree = renderer.create(<Router><Navbar /></Router>).toJSON();
